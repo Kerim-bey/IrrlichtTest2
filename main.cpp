8293d316fe50795ca5f69ccb792673b01c25a8a9
Kotolegokot <gkotolegokot@gmail.com>
2016-09-15 21:01:08 +0300

    Add a quake map

--- main.cpp
+++ main.cpp
@@ -4,11 +4,6 @@
 #include "MyReceiver.h"
 
 using namespace irr;
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
 
 extern int main(int argc, char** argv)
 {
@@ -20,35 +15,77 @@ extern int main(int argc, char** argv)
     if (device == 0) return 1;
     device->setWindowCaption(L"Der'mo");
 
-    IVideoDriver *driver = device->getVideoDriver();
-    ISceneManager *smgr = device->getSceneManager();
-    IGUIEnvironment *guienv = device->getGUIEnvironment();
-    guienv->addStaticText(L"Hello Cruel World! Fuck you...", rect<s32>(10, 10, 260, 22), true);
+    video::IVideoDriver *driver = device->getVideoDriver();
+    scene::ISceneManager *smgr = device->getSceneManager();
+    gui::IGUIEnvironment *guienv = device->getGUIEnvironment();
+    gui::IGUIStaticText *staticText = guienv->addStaticText(L"Hello Cruel World! Fuck you...", core::rect<s32>(10, 10, 300, 22), true);
+    io::IFileSystem *fs = device->getFileSystem();
 
+    // Load quake map
+    fs->addFileArchive("media/maps/map-20kdm2.pk3");
+    scene::IAnimatedMesh *mesh = smgr->getMesh("20kdm2.bsp");
+    scene::ISceneNode *node = nullptr;
+    if (mesh)
+        node = smgr->addOctreeSceneNode(mesh->getMesh(0), 0, -1, 1024);
+    if (node)
+        node->setPosition(core::vector3df(-1400, -110, -1400));
+
+    // Add event receiver
     MyReceiver rec;
     device->setEventReceiver(&rec);
 
-    IAnimatedMesh *mesh = smgr->getMesh("media/models/syndey.md2");
+    // Add mesh
+    scene::IAnimatedMesh *mesh2 = smgr->getMesh("media/models/syndey.md2");
     if (!mesh) {
         device->drop();
         return 1;
     }
-    IAnimatedMeshSceneNode *node = smgr->addAnimatedMeshSceneNode(mesh);
-    if (node) {
-        node->setMaterialFlag(EMF_LIGHTING, false);
-        node->setMD2Animation(scene::EMT_STAND);
-        node->setMaterialTexture(0, driver->getTexture("media/textures/syndey.bmp"));
+    // Add node and attach the mesh to it
+    scene::IAnimatedMeshSceneNode *node2 = smgr->addAnimatedMeshSceneNode(mesh2);
+    if (node2) {
+        //node2->setMaterialFlag(video::EMF_LIGHTING, false);
+        node2->setMD2Animation(scene::EMAT_STAND);
+        node2->setMaterialTexture(0, driver->getTexture("media/textures/syndey.bmp"));
+        node2->setScale(core::vector3df(1.8, 1.8, 1.8));
     }
 
-    ICameraSceneNode *camera = smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+    // Add camera
+    scene::ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS();
+    camera->setPosition(core::vector3df(0, 0, 30));
 
-    while(device->run())
+    // Hide cursor
+    device->getCursorControl()->setVisible(false);
+
+    scene::ILightSceneNode *light = smgr->addLightSceneNode(0, core::vector3df(50.0f,10.0f,0.0f), video::SColorf(1.0f,1.0f,1.0f,1.0f), 500.0f );
+
+    int lastFPS = -1;
+    while(device->run()) {
         if (device->isWindowActive()) {
-            driver->beginScene(true, true, SColor(0,3,243,250));
+            if(rec.IsKeyDown(KEY_KEY_W)) camera->setPosition(camera->getPosition() + core::vector3df(0.1,0,0));
+            if(rec.IsKeyDown(KEY_KEY_S)) camera->setPosition(camera->getPosition() + core::vector3df(-0.1,0,0));
+            if(rec.IsKeyDown(KEY_KEY_A)) camera->setPosition(camera->getPosition() + core::vector3df(0,0,0.1));
+            if(rec.IsKeyDown(KEY_KEY_D)) camera->setPosition(camera->getPosition() + core::vector3df(0,0,-0.1));
+
+            int fps = driver->getFPS();
+            if (lastFPS != fps)
+            {
+                core::stringw str = L"Irrlicht Engine - Quake 3 Map example [";
+                str += driver->getName();
+                str += "] FPS:";
+                str += fps;
+
+                device->setWindowCaption(str.c_str());
+                lastFPS = fps;
+                staticText->setText(str.c_str());
+            }
+
+            driver->beginScene(true, true, video::SColor(0,3,243,250));
             smgr->drawAll();
             guienv->drawAll();
             driver->endScene();
-        }
+        } else
+            device->yield();
+    }
 
     device->drop();
 
